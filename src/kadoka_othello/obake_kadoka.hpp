53092e2294b8ea3e3e6d671f06fbad72a965a60b
kadoka <tomiya3499@gmail.com>
2026-09-11 20:50:27 +0900

    Refine Obake Kadoka evaluation and randomizer interface

--- src/kadoka_othello/obake_kadoka.hpp
+++ src/kadoka_othello/obake_kadoka.hpp
@@ -11,12 +11,18 @@
 namespace kadoka::othello {
 
 struct ObakeKadokaConfig {
-    double corner_weight{5.0};
-    double edge_weight{2.0};
-    double near_piece_weight{1.2};
-    double center_weight{0.4};
+    double corner_score{8.0};
+    double edge_score{2.0};
+    double x_square_penalty{7.0};
+    double c_square_penalty{4.0};
+    double occupied_neighbor_score{0.7};
+    double empty_neighbor_penalty{0.25};
+    double mixed_color_score{1.2};
+    double line_potential_score{1.6};
+    double center_early_score{0.5};
     double recent_retry_penalty{0.05};
-    double exploration_floor{0.15};
+    double exploration_floor{0.02};
+    double randomizer_temperature{3.0};
     std::size_t memory_depth{2};
 };
 
@@ -33,13 +39,15 @@ public:
 private:
     struct WeightedMove {
         Position move{};
+        double score{};
         double weight{};
     };
 
     [[nodiscard]] std::size_t collect_candidates(
         const Board& board,
         std::array<WeightedMove, 100>& candidates) const;
-    [[nodiscard]] double score_position(const Board& board, Position move) const;
+    [[nodiscard]] double evaluate_position(const Board& board, Position move) const;
+    [[nodiscard]] double score_to_weight(double score, bool recent) const noexcept;
     [[nodiscard]] bool is_recent(Position move) const noexcept;
     [[nodiscard]] Position choose_weighted(
         const std::array<WeightedMove, 100>& candidates,
