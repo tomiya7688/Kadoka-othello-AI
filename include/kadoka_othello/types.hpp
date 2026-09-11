35a331c3322e58c4500f80ec1add5f5dc49898d8
kadoka <tomiya3499@gmail.com>
2026-09-11 20:16:29 +0900

    Add core Othello types

---
+++ include/kadoka_othello/types.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+namespace kadoka::othello {
+
+enum class Cell : std::uint8_t {
+    Empty = 0,
+    Black = 1,
+    White = 2,
+};
+
+enum class Player : std::uint8_t {
+    Black = 1,
+    White = 2,
+};
+
+struct Position {
+    std::size_t row{};
+    std::size_t col{};
+
+    friend constexpr bool operator==(const Position&, const Position&) = default;
+};
+
+struct Move {
+    Player player{Player::Black};
+    Position position{};
+};
+
+constexpr Cell to_cell(Player player) noexcept {
+    return player == Player::Black ? Cell::Black : Cell::White;
+}
+
+constexpr Player opponent(Player player) noexcept {
+    return player == Player::Black ? Player::White : Player::Black;
+}
+
+}  // namespace kadoka::othello
