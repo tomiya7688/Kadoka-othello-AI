489c16894ff06e46e5090c395e433f5bf0e11f1a
kadoka <tomiya3499@gmail.com>
2026-09-11 21:12:26 +0900

    Connect script evaluator runtime to model assets

--- src/kadoka_othello/script_evaluator.hpp
+++ src/kadoka_othello/script_evaluator.hpp
@@ -5,6 +5,8 @@
 #include <utility>
 #include <vector>
 
+#include "kadoka_othello/model_descriptor.hpp"
+
 namespace kadoka::othello {
 
 struct ScriptEvaluatorFeature {
@@ -58,5 +60,9 @@ private:
 [[nodiscard]] ScriptEvaluatorConfig load_script_evaluator_config(
     const std::string& path,
     const std::string& default_script_path = {});
+
+[[nodiscard]] ScriptEvaluator load_script_evaluator_asset(
+    const ModelRootDescriptor& model,
+    const std::string& asset_id);
 
 }  // namespace kadoka::othello
