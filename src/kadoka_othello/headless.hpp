0de66ae1e2ced8948c57c6753fd4aee2b4c56023
kadoka <tomiya3499@gmail.com>
2026-09-11 20:29:40 +0900

    Make headless runner accept AI packages

--- src/kadoka_othello/headless.hpp
+++ src/kadoka_othello/headless.hpp
@@ -4,6 +4,7 @@
 #include <cstdint>
 #include <iosfwd>
 
+#include "kadoka_othello/ai.hpp"
 #include "kadoka_othello/state.hpp"
 
 namespace kadoka::othello {
@@ -12,6 +13,7 @@ struct HeadlessConfig {
     std::size_t board_size{8};
     std::size_t games{1};
     std::uint64_t seed{0};
+    std::size_t max_invalid_attempts_per_turn{1024};
     bool write_json_lines{true};
 };
 
@@ -20,7 +22,14 @@ struct HeadlessSummary {
     std::size_t black_wins{};
     std::size_t white_wins{};
     std::size_t draws{};
+    std::size_t invalid_move_attempts{};
 };
+
+[[nodiscard]] HeadlessSummary run_games(
+    const HeadlessConfig& config,
+    AIPackage black,
+    AIPackage white,
+    std::ostream* dataset_output = nullptr);
 
 [[nodiscard]] HeadlessSummary run_random_games(
     const HeadlessConfig& config,
